fd7e2d57676e3b62a9dd6b6a5974c607eee62a05
Max Ng <maxwindiff@gmail.com>
2025-01-02 20:37:04 -0800

    Initial Commit

---
+++ vision06/vision06App.swift
@@ -0,0 +1,33 @@
+//
+//  vision06App.swift
+//  vision06
+//
+//  Created by Max Ng on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct vision06App: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
