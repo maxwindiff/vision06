7c5b410d1fd6bc9ebbd9a4c79e40c3c70642b0be
Max Ng <maxwindiff@gmail.com>
2025-01-23 23:42:13 -0800

    simplification

--- vision06/Trails/Trail.swift
+++ vision06/Trails/Trail.swift
@@ -3,17 +3,13 @@ import Foundation
 import RealityKit
 
 public struct Trail {
-  let rootEntity: Entity
-  var curveExtruder: CurveExtruder
   var smoothCurveSampler: SmoothCurveSampler
-  var inputsOverTime: Deque<(SIMD3<Float>, TimeInterval)> = []
 
   @MainActor
   init(rootEntity: Entity) async {
-    curveExtruder = CurveExtruder(shape: makeCircle(radius: 1, segmentCount: Int(32)))
+    let curveExtruder = CurveExtruder(shape: makeCircle(radius: 1, segmentCount: Int(32)))
     smoothCurveSampler = SmoothCurveSampler(flatness: 0.001, extruder: curveExtruder)
 
-    self.rootEntity = rootEntity
     let solidMeshEntity = Entity()
     rootEntity.addChild(solidMeshEntity)
     solidMeshEntity.position = .zero
@@ -22,17 +18,6 @@ public struct Trail {
 
   @MainActor
   mutating func receive(input: SIMD3<Float>, time: TimeInterval) {
-    // Remove old entries from deque, insert current position if changed.
-    while let (_, headTime) = inputsOverTime.first, time - headTime > 0.1 {
-      inputsOverTime.removeFirst()
-    }
-    let lastInputPosition = inputsOverTime.last?.0
-    inputsOverTime.append((input, time))
-    if let lastInputPosition, lastInputPosition == input {
-      return
-    }
-    // TODO: maybe remove minor movements?
-
     smoothCurveSampler.trace(point: input)
   }
 }
